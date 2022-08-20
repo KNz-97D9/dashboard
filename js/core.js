ba5e50d83db48efd72cc846df000dffdfbb67727
KNz-97D9 <111625124+KNz-97D9@users.noreply.github.com>
2022-08-21 01:30:48 +0900

    出力文言の変更

--- js/core.js
+++ js/core.js
@@ -2,6 +2,6 @@ const urlPathname = document.location.pathname;
 let targetElem = document.getElementsByTagName('main')[0];
 var addElement = document.createElement('div');
 addElement.setAttribute('id', 'aux_function');
-var addContent = document.createTextNode('Hello, world! ' + urlPathname);
+var addContent = document.createTextNode('Hello, world!<br>' + urlPathname);
 addElement.appendChild(addContent);
 targetElem.insertBefore(addElement, targetElem.firstChild);
