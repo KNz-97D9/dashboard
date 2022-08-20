9b00c0edda98c2a85ab88e64830088bdcf1d0565
KNz-97D9 <111625124+KNz-97D9@users.noreply.github.com>
2022-08-21 01:20:15 +0900

    中核となるJavaScriptファイルの新規作成

---
+++ js/core.js
@@ -0,0 +1,7 @@
+const urlPathname = document.location.pathname;
+let targetElem = document.getElementsByTagName('main')[0];
+var addElement = document.createElement('div');
+addElement.setAttribute('id', 'aux_function');
+var addContent = document.createTextNode('Hello, world! ' + urlPathname);
+addElement.appendChild(addContent);
+targetElem.insertBefore(addElement, targetElem.firstChild);
