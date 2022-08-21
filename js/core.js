511d2dca5fd30bcd1d0e3a7f72b04baa976e3f6c
KNz-97D9 <111625124+KNz-97D9@users.noreply.github.com>
2022-08-21 23:20:55 +0900

    loadDashboard関数の追加

--- js/core.js
+++ js/core.js
@@ -1,7 +1,45 @@
-const urlPathname = document.location.pathname;
-let targetElem = document.getElementsByTagName('main')[0];
-var addElement = document.createElement('div');
-addElement.setAttribute('id', 'aux_function');
-var addContent = document.createTextNode('Hello, world!' + urlPathname);
-addElement.appendChild(addContent);
-targetElem.insertBefore(addElement, targetElem.firstChild);
+const URL_ARRAY = {
+  base_html: 'https://cdn.jsdelivr.net/gh/KNz-97D9/dashboard@v1.0.2/base.html'
+};
+
+/**
+ * 
+ */
+function loadDashboard(url = '', parentElement = document.body) {
+
+  // 
+  var nodes = fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      return new DOMParser().parseFromString(data, 'text/html');
+    });
+  
+  // ノードを指定した親要素の先頭に挿入
+  parentElement.prepend(nodes);
+}
+
+// ダッシュボードを読み込む
+loadDashboard(URL_ARRAY['base_html'], document.getElementsByTagName('main')[0]);
+
+/**
+ * 
+ */
+function executeDashboardScripts() {
+
+  // 
+  const urlPathname = document.location.pathname;
+
+  // 
+  let targetElem = document.getElementsByTagName('main')[0];
+
+  var addElement = document.createElement('div');
+  addElement.setAttribute('id', 'aux_function');
+
+  var addContent = document.createTextNode('Hello, world!' + urlPathname);
+  addElement.appendChild(addContent);
+
+  targetElem.insertBefore(addElement, targetElem.firstChild);
+}
+
+// 
+// executeDashboardScripts();
